3a72e1c52ca0d3ec1ee2bf1d1d572d09c3f80614
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-09-25 18:40:38 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import os
-import test.hash as hash
-import test.SQLite as DB
+import Modules.hash as hash
+import Modules.SQLite as DB
 import questionary
 from dotenv import load_dotenv, set_key, find_dotenv
 
